3da97f7d0df1eea232482ff2ea8161bd479cb95d
ardaktileu <Ardak.Tileu@erg.kz>
2024-10-07 21:13:01 +0500

    refactor: remove the redudant comments

--- src/main.tsx
+++ src/main.tsx
@@ -5,13 +5,11 @@ import { routeTree } from './routeTree.gen'
 import "normalize.css"
 import "./app/styles/index.scss"
 
-// Set up a Router instance
 const router = createRouter({
   routeTree,
   defaultPreload: 'intent',
 })
 
-// Register things for typesafety
 declare module '@tanstack/react-router' {
   interface Register {
     router: typeof router
