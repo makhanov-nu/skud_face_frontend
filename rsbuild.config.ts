9f2282eaed7ef984c55315e85467aa71425234c4
ardaktileu <Ardak.Tileu@erg.kz>
2024-10-07 14:58:02 +0500

    chore: set up the react router

---
+++ rsbuild.config.ts
@@ -0,0 +1,18 @@
+import { defineConfig } from '@rsbuild/core'
+import { pluginReact } from '@rsbuild/plugin-react'
+import { TanStackRouterRspack } from '@tanstack/router-plugin/rspack'
+import { pluginSass } from '@rsbuild/plugin-sass'
+
+export default defineConfig({
+  plugins: [pluginReact(), pluginSass()],
+  source: {
+    entry: { index: './src/main.tsx' },
+  },
+  tools: {
+    rspack: {
+      plugins: [
+        TanStackRouterRspack(),
+      ],
+    },
+  },
+})
---
+++ src/routes/index.tsx
@@ -0,0 +1,26 @@
+import * as React from 'react'
+import { createFileRoute } from '@tanstack/react-router'
+import { Button, Col, Row, Typography } from 'antd';
+
+const { Title } = Typography;
+
+export const Route = createFileRoute('/')({
+  component: HomeComponent,
+})
+
+function HomeComponent() {
+  function onClick() {
+    alert('clicked!')
+  }
+
+  return (
+    <div className="p-2">
+    <Row>
+      <Col>
+        <Title level={2}>Skud Face</Title>
+        <Button onClick={onClick}>Click me!</Button>
+      </Col>
+    </Row>
+    </div>
+  )
+}
