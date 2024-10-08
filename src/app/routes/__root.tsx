dbab6ac054785588ddecbe268668486bfd399764
ardaktileu <Ardak.Tileu@erg.kz>
2024-10-09 00:56:26 +0500

    refactor: hide the devtools in production

--- src/app/routes/__root.tsx
+++ src/app/routes/__root.tsx
@@ -1,6 +1,17 @@
 import * as React from 'react';
 import { Link, Outlet, createRootRoute } from '@tanstack/react-router';
-import { TanStackRouterDevtools } from '@tanstack/router-devtools';
+
+const TanStackRouterDevtools =
+	process.env.NODE_ENV === 'production'
+		? () => null // Render nothing in production
+		: React.lazy(() =>
+				// Lazy load in development
+				import('@tanstack/router-devtools').then((res) => ({
+					default: res.TanStackRouterDevtools,
+					// For Embedded Mode
+					// default: res.TanStackRouterDevtoolsPanel
+				})),
+			);
 
 export const Route = createRootRoute({
 	component: RootComponent,
