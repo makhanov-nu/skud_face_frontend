8e9248589e9921bba39a589874c98d49aef0db60
ardaktileu <Ardak.Tileu@erg.kz>
2024-10-10 20:29:28 +0500

    feat: add the Open Sans font

--- src/app/routes/__root.tsx
+++ src/app/routes/__root.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { Link, Outlet, createRootRoute } from '@tanstack/react-router';
+import { Outlet, createRootRoute } from '@tanstack/react-router';
 
 const TanStackRouterDevtools =
 	process.env.NODE_ENV === 'production'
@@ -20,25 +20,6 @@ export const Route = createRootRoute({
 function RootComponent() {
 	return (
 		<>
-			<div className="p-2 flex gap-2 text-lg">
-				<Link
-					to="/"
-					activeProps={{
-						className: 'font-bold',
-					}}
-					activeOptions={{ exact: true }}
-				>
-					Home
-				</Link>{' '}
-				<Link
-					to="/about"
-					activeProps={{
-						className: 'font-bold',
-					}}
-				>
-					About
-				</Link>
-			</div>
 			<hr />
 			<Outlet />
 			<TanStackRouterDevtools position="bottom-right" />
