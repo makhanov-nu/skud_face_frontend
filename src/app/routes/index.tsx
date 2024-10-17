a27251a8a92b16f638040dc946791c9e18a7f954
ardaktileu <Ardak.Tileu@erg.kz>
2024-10-17 18:04:17 +0500

    refactor: change the fallback route

--- src/app/routes/index.tsx
+++ src/app/routes/index.tsx
@@ -3,7 +3,7 @@ import { LoginPage } from '@/pages/login';
 import { isAuthenticated } from '@/entities/session';
 import { z } from 'zod';
 
-const fallback = '/organizations/register' as const;
+const fallback = '/organizations/create' as const;
 
 export const Route = createFileRoute('/')({
 	validateSearch: z.object({
